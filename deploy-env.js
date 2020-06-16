69a78d7e8705507ac0ee39fcdfb6fb097f9bbb1f
Tine Kleivane <kleivane@gmail.com>
2020-06-16 20:43:20 +0200

    Switch from ghactions to npm scripts

--- deploy-env.js
+++ deploy-env.js
@@ -1,16 +1,26 @@
-const fs = require('fs');
+const HOST_BUCKET_NAME='tf-2-host-test'
+const VERSION='1'
+const environment = process.argv[2];
 
-const sha = process.env.GITHUB_SHA || 1;
-const environment =  "test";
-const url = 'https://tf-2-assets.s3.eu-north-1.amazonaws.com';
+var AWS = require('aws-sdk');
+var fs = require('fs');
+
+
+const url = '';
+const sha =VERSION;
 const date = new Date().toISOString();
+
+console.log(`ENV_NAME: '${environment}''`)
+console.log(`GIT_SHA: '${sha}'`)
+console.log(`API_URL: '${url}'`)
+console.log(`CREATED_AT: '${date}'`)
 
 const index = `<!doctype html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Immutable webapp starterpack</title>
+    <title>Immutable webapp</title>
     <meta name="description" content="Immutable webapp.">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   </head>
@@ -24,11 +34,26 @@ const index = `<!doctype html>
            CREATED_AT: '${date}'
        }
        </script>
+
        <!-- application binding -->
        <app-root></app-root>
        <!-- fully-qualified static assets -->
        <script src="${url}/assets/${sha}/main.js" type="text/javascript"></script>
-   </body>
-</html>`;
 
-fs.writeFile('index.html', index, 'utf8', function(){});
+
+   </body>
+</html>`
+
+var s3 = new AWS.S3();
+    var params = {
+        Bucket : HOST_BUCKET_NAME,
+        Key : 'index.html',
+        Body : index,
+        CacheControl: 'no-store',
+        ContentType: "text/html"
+    }
+
+s3.putObject(params, function(err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else     console.log(data);
+});
